16c6c557f1db9f395be634d40308ce778a5ad17f
Anıl Başaran <nlbsrn@gmail.com>
2024-09-17 21:32:30 +0300

    added some fetch capabilities

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import './App.css';
+import {useState} from "react";
 import Joke from "./Jokes.jsx";
+import clsx from "clsx";
 
-const JOKE = {
-  question: "Why don't eggs tell jokes?",
-  answer: "They'd crack each other up.",
-};
 
 const App = () => {
 
-  
+  const [joke,setJoke] = useState(null);
+
+  const fetchJoke=()=>{
+    fetch("https://api.react-formula.com/learning-api/demos/random-joke/jokes")
+    .then((response)=>response.json())
+    .then((data)=>setJoke(data))
+  }
   
   return <div className=" flex h-screen justify-center items-center">
 
-    <div className="w-full max-w-xl flex justify-center items-center">
-    <Joke joke={JOKE} />
+    <div className="w-full max-w-xl flex flex-col justify-center items-center">
+    <Joke joke={joke} />
+
+      <button
+        onClick={()=>fetchJoke()}
+        className={clsx("bg-blue-400 rounded-lg p-2 w-36",joke ? "text-black" : "text-white m-24" )}>New Joke</button>
     </div>
     
   </div>;
--- src/Jokes.jsx
+++ src/Jokes.jsx
@@ -1,33 +1,30 @@
-import {useState} from "react";
+import { useState } from "react";
 
-const Jokes=(props)=>{
+const Jokes = (props) => {
+  const [showAnswer, setShowAnswer] = useState(true);
 
+  const { joke } = props;
 
-  const [showAnswer,setShowAnswer] = useState(true);
-  const {joke} = props;
-  
-  return(
-
-    <div className="flex flex-col items-center ">
-      
-        <div className="text-2xl" >{joke.question}</div>
-
-      {showAnswer ? (<div className="h-6 m-4 text-blue-300 text-xl">{joke.answer}</div>)
-      :
-      
-      (<div className="m-4 h-6"></div>)
-      
-      }
-        
-        <button
-          onClick={()=>setShowAnswer(!showAnswer)}
-          className="bg-green-400 rounded-lg p-2 m-2">Reveal Answer</button>
-    
-
+  return (
+    <div className="flex flex-col items-center">
+      {joke ? (
+        <>
+          <div className="text-2xl">{joke.question}</div>
+          {showAnswer ? (
+            <div className="h-6 m-4 text-blue-300 text-xl">{joke.answer}</div>
+          ) : (
+            <div className="m-4 h-6"></div>
+          )}
+          <button
+            onClick={() => setShowAnswer(!showAnswer)}
+            className="bg-green-400 rounded-lg p-2 m-2 w-36"
+          >
+            {showAnswer ? "Hide Answer" : "Reveal Answer"}
+          </button>
+        </>
+      ) : null}
     </div>
-    
   );
-  
 };
 
 export default Jokes;
